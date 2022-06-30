aa1b64df21545dbf3a40c91a198548109717ff70
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-06-30 10:45:48 -0500

    Updated homepage

--- list/urls.py
+++ list/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('snes/', views.snes, name='snes'),
-    path('neogeo/', views.neogeo, name='neogeo'),
+    path('', views.index),
+    path('snes/', views.snes),
+    path('neogeo/', views.neogeo),
 ]
